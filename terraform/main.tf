74df7c4465a98e688aa61e192dc33dfbb1ed8996
Kyle Hancock <kyle.hancock@jasprhealth.com>
2025-11-17 12:15:30 -0500

    remove unneeded root domain record.

--- terraform/main.tf
+++ terraform/main.tf
@@ -222,18 +222,6 @@ resource "aws_s3_bucket_policy" "cloudfront_access" {
 }
 
 # Route53 Records
-resource "aws_route53_record" "apex" {
-  zone_id = data.aws_route53_zone.this.zone_id
-  name    = var.domain
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.website.domain_name
-    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.this.zone_id
   name    = var.fqdn
