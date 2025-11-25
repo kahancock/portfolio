1271cf35f34979d0ea224364f6090a5cf732d71c
Kyle Hancock <kyle@kylehancock.com>
2025-11-24 19:05:54 -0500

    update default region

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "eastus2"
 }
 
 variable "static_web_app_name" {
